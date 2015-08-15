34d3aa9aa92c0fe7915307ff0808b2504bfccda9
alexschilpp <alexschilpp@users.noreply.github.com>
2015-08-15 18:51:42 +0200

    Add page slug to body class

--- functions.php
+++ functions.php
@@ -49,4 +49,14 @@ register_nav_menus(array(
 	'footer' => ('Footer Menu'),
 ));
 
+// Add Page Slug Body Class
+function add_slug_body_class( $classes ) {
+	global $post;
+	if ( isset( $post ) ) {
+	$classes[] = $post->post_type . '-' . $post->post_name;
+	}
+	return $classes;
+}
+add_filter( 'body_class', 'add_slug_body_class' );
+
 ?>
